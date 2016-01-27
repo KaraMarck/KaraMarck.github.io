1e1b649e351742cee1f750d01498f7aa63f82c9d
Kara de la Marck <karadelamarck@gmail.com>
2016-01-27 20:27:14 +0000

    Made a To Do List

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 /* Exercise 1: Wish list */
+function addToList(item) {
+	$("#items").append("<li>" + item + "<span class='label pending'>Pending</span></li>")
+}
+
+$(document).on('click', '#add-to-list', function() {
+	addToList($("#item").val());
+	$("#item").val("");
+	$( "#item" ).focus();
+	updateTotal();
+});
+
+$(document).on('click', '.pending', function() {
+	var li_node = $(this).parent();
+	li_node.append("<span class='label success'>Done!</span>");
+	$(this).remove();
+	li_node.addClass('completed');
+	updateTotal();
+});
+
+function updateTotal() {
+	var pendingItems = $(".pending").length;
+	var completedItems = $(".completed").length;
+	$(".total").text('Pending: ' + pendingItems + ' Completed: ' + completedItems);
+}
